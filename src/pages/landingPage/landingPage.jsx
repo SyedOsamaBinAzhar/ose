29b7bc92f4c6e39b999c8f115c0ede3d86e7db74
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2022-02-16 15:35:51 +0500

    about section heading layout ready.

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '../../components/heroSection/navbar/navbar';
 import "./landingPage.css";
+import "./aboutSection.css";
+import "./heroSection.css";
 
 const LandingPage = () => {
   return (
@@ -12,7 +14,14 @@ const LandingPage = () => {
                 <p>Deals in : All kinds of industrail tools, fabrics, garments accessories, CMT manufacturing & general order suppliers .</p>
             </div>
         </div>
-        <div className="aboutSection">ABOUT</div>
+        <div className="aboutSection">
+            <div className="headingRow flex-col">
+                <h1>About Us</h1>
+                <div className="line"></div>
+                <p>Established in 2021, we, One Step Enterprises (OSE), are a modern company, up to date with all the recent equipment and required knowledge of business needs. With us, you get to buy several instruments in one place.</p>
+            </div>
+            <div className="contentRow"></div>
+        </div>
         <div className="whatWeOfferSection">whatWeOfferSection</div>
         <div className="clientsSection">clientsSection</div>
         <div className="footerSection">footerSection</div>

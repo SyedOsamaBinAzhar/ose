b8995edc865d83874b4f476678851227b262568b
SyedOsamaBinAzhar <osamaazhar.edu@gmail.com>
2022-02-16 15:17:49 +0500

    hero section layout structure completed.

--- src/components/heroSection/navbar/navbar.jsx
+++ src/components/heroSection/navbar/navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./navbar.css";
+import logo from "../../../assets/landingPage/heroSection/logoNav.svg";
 
 const Navbar = () => {
   return (
     <div className='navbarCont'>
+        <div className="logoCont">
+            <img src= {logo} alt = ''/>
+        </div>
         <div className="navItemsCont">
           <li>Home</li>
           <li>About Us</li>
--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -7,7 +7,10 @@ const LandingPage = () => {
     <div className='landingPageContainer'>
         <div className="heroSection">
             <div className="navbarCont"><Navbar/></div>
-            <div className="contentCont">contentCont</div>
+            <div className="contentCont flex-col">
+                <h1>One Step Enterprises</h1>
+                <p>Deals in : All kinds of industrail tools, fabrics, garments accessories, CMT manufacturing & general order suppliers .</p>
+            </div>
         </div>
         <div className="aboutSection">ABOUT</div>
         <div className="whatWeOfferSection">whatWeOfferSection</div>
